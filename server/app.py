60e16681507224552fdd65076cb5f876434c37fe
Grace Wambui Njau <gracenjau2005@gmail.com>
2024-07-16 18:55:14 +0300

    python-p4-flask-restful-crud-lab

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -16,40 +14,51 @@ db.init_app(app)
 
 api = Api(app)
 
-
-class Plants(Resource):
-
+class PlantList(Resource):
     def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+        plants = Plant.query.all()
+        plants_data = [p.to_dict() for p in plants]
+        return make_response(jsonify(plants_data), 200)
 
     def post(self):
         data = request.get_json()
-
         new_plant = Plant(
             name=data['name'],
             image=data['image'],
-            price=data['price'],
+            price=data['price']
         )
-
         db.session.add(new_plant)
         db.session.commit()
-
         return make_response(new_plant.to_dict(), 201)
 
+api.add_resource(PlantList, '/plants')
 
-api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
-
+class PlantDetail(Resource):
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        return make_response(jsonify({"error": "Plant not found"}), 404)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            data = request.get_json()
+            for key, value in data.items():
+                setattr(plant, key, value)
+            db.session.commit()
+            return make_response(jsonify(plant.to_dict()), 200)
+        return make_response(jsonify({"error": "Plant not found"}), 404)
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response({}, 204)
+        return make_response(jsonify({"error": "Plant not found"}), 404)
 
+api.add_resource(PlantDetail, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
